1dd3804817110d458b4467961162c3998e2481d1
Jadielson Santos <jadielson.js4@gmail.com>
2020-04-27 20:47:24 -0300

    Captura Ip

--- Web.py
+++ Web.py
@@ -3,13 +3,13 @@ system("clear")
 try:
     if (path.exists(".ps.ngrok") == True):
         system("clear")
-        print("INICIE O NGROK NA PORTA 8000\nCOMANDO E ESSE AI EM OUTRA ABA\ncd .ps.ngrok && chmod 777 ngrok && ./ngrok http 8000\n")
+        print("INICIE O NGROK NA PORTA 8000\nCOMANDO E ESSE AI EM OUTRA ABA NO MESMO DIRETORIO (Face-Web)\ncd .ps.ngrok && chmod 777 ngrok && ./ngrok http 8000\n")
     else:
         system("apt install git")
-        system("cd && git clone https://github.com/PSecurity/ps.ngrok")
+        system("git clone https://github.com/PSecurity/ps.ngrok")
         system("mv -v ps.ngrok .ps.ngrok")
         system("clear")
-        print("INICIE O NGROK QUE JA FOI INSTALADO NO SEU TERMINAL\nCOMANDO E ESSE AI, EM OUTRA ABA\n cd .ps.ngrok && chmod 777 ngrok && ./ngrok http 8000")
+        print("INICIE O NGROK QUE JA FOI INSTALADO NO SEU TERMINAL\nCOMANDO E ESSE AI, EM OUTRA ABA NO MESMO DIRETORIO (Face-Web)\n cd .ps.ngrok && chmod 777 ngrok && ./ngrok http 8000")
 except:
     print("Erro")
 
