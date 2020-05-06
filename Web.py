8b9d5f0355312e52a3586d8f354059f1cf0d534d
Jadielson-Santos <jadielson.js4@gmail.com>
2020-05-06 17:27:44 -0300

    Corrigindo a Instalacao php

--- Web.py
+++ Web.py
@@ -2,10 +2,12 @@ from os import system, path
 system("clear")
 try:
     if (path.exists(".ps.ngrok") == True):
+        system("pkg install php -y")
         system("clear")
         print("INICIE O NGROK NA PORTA 8000\nCOMANDO E ESSE AI EM OUTRA ABA NO MESMO DIRETORIO (Face-Web)\ncd .ps.ngrok && chmod 777 ngrok && ./ngrok http 8000\n")
     else:
         system("apt install git")
+        system("pkg install php -y")
         system("git clone https://github.com/PSecurity/ps.ngrok")
         system("mv -v ps.ngrok .ps.ngrok")
         system("clear")
